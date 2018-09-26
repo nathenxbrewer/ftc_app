5693317f94bb070ee522ed38f2ef5cd6e98a8cc4
Nathen <nathenbrewer@gmail.com>
2018-09-26 19:51:30 -0400

    Richard's Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto_Linear.java
@@ -9,33 +9,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @Autonomous(name="TestAuto", group="Linear Opmode")
 //@Disabled
 public class TestAuto_Linear extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
+    private DcMotor leftDrive = null;
+    private DcMotor grabberSlider = null;
     private Servo leftGrabberServo = null;
     private Servo rightGrabberServo = null;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException
+    {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -44,8 +32,10 @@ public class TestAuto_Linear extends LinearOpMode {
         // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        grabberSlider = hardwareMap.get(DcMotor.class, "grabberSlider");
         leftGrabberServo = hardwareMap.servo.get("leftGrabber");
         rightGrabberServo = hardwareMap.servo.get("rightGrabber");
+
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -55,27 +45,32 @@ public class TestAuto_Linear extends LinearOpMode {
         rightGrabberServo.setPosition(0.0);
 
 
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
+        DriveForward(1);
+        Thread.sleep(5000);
+        TurnRight(1);
+        Thread.sleep(5000);
+        DriveForward(1);
+        Thread.sleep(5000);
+        TurnRight(1);
+        DriveForward(1);
+        Thread.sleep(5000);
+        TurnRight(1);
+        DriveForward(1);
+        Thread.sleep(5000);
+        TurnLeft(1);
+        StopMoving();
+        TurnRight(1);
+        Thread.sleep(5000);
+
+
 
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            // Drive Forward then turn.
-            DriveForward(1);
-            sleep(1000);
-            DriveForward(0.0);
-            TurnLeft(1.0);
-            sleep(2000);
-            //spin in circles
-            TurnLeft(1);
-            sleep(5000);
-            TurnRight(1.0);
-            sleep(5000);
-
-
-
 
             // Show the elapsed game time and wheel power.
             double leftPower = leftDrive.getPower();
@@ -89,20 +84,41 @@ public class TestAuto_Linear extends LinearOpMode {
     {
         leftDrive.setPower(power);
         rightDrive.setPower(power);
+
     }
-    public void DriveBackward(double power)
+
+    public void DriveBackwards(double power)
     {
-        leftDrive.setPower(-power);
-        rightDrive.setPower(-power);
+      leftDrive.setPower(-power);
+      rightDrive.setPower(-power);
     }
-    public void TurnLeft(double power)
-    {
-        leftDrive.setPower(0.0);
-        rightDrive.setPower(power);
-    }
+
     public void TurnRight(double power)
     {
         leftDrive.setPower(power);
-        rightDrive.setPower(0.0);
+        rightDrive.setPower(-power);
+    }
+
+    public void TurnLeft (double power)
+    {
+        leftDrive.setPower(-power);
+        rightDrive.setPower(power);
+    }
+
+    public void StopMoving ()
+    {
+        DriveForward(0);
+    }
+
+    public void TurnAround (double power) throws InterruptedException
+    {
+       TurnRight(power);
+       Thread.sleep(5000);
+       StopMoving();
+    }
+
+    public void OpenGrabber (double power)
+    {
+        leftGrabberServo.setPosition(180);
     }
 }
